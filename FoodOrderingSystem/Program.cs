5f4f728c9bf7f4c755807300d6c50c8467d74f6c
Alisasajdulina <alisasajdulina9@gmail.com>
2025-12-17 15:01:27 +0500

    17.12

--- FoodOrderingSystem/Program.cs
+++ FoodOrderingSystem/Program.cs
@@ -2,12 +2,19 @@ using FoodOrderingSystem.Repositories;
 using FoodOrderingSystem.Services;
 using FoodOrderingSystem.Profiles;
 using FoodOrderingSystem.Middleware;
+using FoodOrderingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<RestaurantService>();
+
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
--- FoodOrderingSystem/Repositories/Repository.cs
+++ FoodOrderingSystem/Repositories/Repository.cs
@@ -19,29 +19,23 @@ public class Repository<T> : IRepository<T> where T : class
         _dbSet = context.Set<T>();
     }
 
-    /// <summary>Получить все записи</summary>
     public async Task<IEnumerable<T>> GetAllAsync() =>
         await _dbSet.ToListAsync();
 
-    /// <summary>Получить запись по id</summary>
     public async Task<T?> GetByIdAsync(int id) =>
         await _dbSet.FindAsync(id);
 
-    /// <summary>Добавить запись</summary>
     public async Task AddAsync(T entity)
     {
         await _dbSet.AddAsync(entity);
         await _context.SaveChangesAsync();
     }
 
-    /// <summary>Обновить запись</summary>
     public async Task UpdateAsync(T entity)
     {
         _dbSet.Update(entity);
         await _context.SaveChangesAsync();
     }
-
-    /// <summary>Удалить запись</summary>
     public async Task DeleteAsync(int id)
     {
         var entity = await GetByIdAsync(id);
